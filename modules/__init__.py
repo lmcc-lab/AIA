d5ad37fcb57d90f39e9d6c3470a2f9cb0b3da2f9
lmcc-lab <lmproductions2014@gmail.com>
2022-09-19 12:06:30 +1000

    Hugging face initialisation working

--- modules/__init__.py
+++ modules/__init__.py
@@ -14,9 +14,10 @@ from diffusers.schedulers.scheduling_ddim import DDIMScheduler
 from transformers import CLIPTextModel, CLIPTokenizer
 from tqdm.auto import tqdm
 from huggingface_hub import notebook_login
-from google.colab import output
 
 device = 'cuda'
 
-output.enable_custom_widget_manager()
-notebook_login()
+pipe = StableDiffusionPipeline.from_pretrained(
+    'CompVis/stable-diffusion-v1-4', revision='fp16',
+    torch_dtype=torch.float16, use_auth_token=True)
+pipe = pipe.to(device)
